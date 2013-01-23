ca59d3476198159de69c3ff4ab2d698d55602b71
Igar Sushko <igar.sushko@gmail.com>
2013-01-23 21:12:14 +0300

    Initial version of PlainQ.

---
+++ src/main/java/com/elasticjogger/plainq/Runner.java
@@ -0,0 +1,15 @@
+package com.elasticjogger.plainq;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Runner
+{
+
+  static final Logger logger = LoggerFactory.getLogger(Runner.class);
+
+  public static void main(String args[])
+  {
+    logger.info("hello");
+  }
+}
