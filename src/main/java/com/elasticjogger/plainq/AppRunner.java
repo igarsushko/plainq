efda6bc6fb4bd9100ec361e78d0c4b20f9520567
Igar Sushko <igar.sushko@gmail.com>
2013-01-24 22:05:15 +0300

    Simplified runner class.

--- src/main/java/com/elasticjogger/plainq/AppRunner.java
+++ src/main/java/com/elasticjogger/plainq/AppRunner.java
@@ -7,33 +7,44 @@ import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.List;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.ConnectionMetaData;
 
 public class AppRunner
 {
 
-  public static void main(String[] args) throws Exception
+  ClassLoader mClassLoader;
+
+  public AppRunner()
   {
-    AppRunner runner = new AppRunner();
-    runner.start();
+    mClassLoader = this.getClass().getClassLoader();
   }
 
-  /**
-   * Starts application and adds shutdown hook.
-   */
-  public void start() throws Exception
+  public static void main(String[] args) throws Exception
   {
     List<URL> urls = listJars("../temp/lib");
     urls.addAll(listJars("target/runtime/plainq/lib"));
     ClassLoader classloader = new URLClassLoader(urls.toArray(new URL[urls.size()]), ClassLoader.getSystemClassLoader().getParent());
 
-    Class<?> runnerClass = classloader.loadClass("com.elasticjogger.plainq.Runner");
+    Class<?> runnerClass = classloader.loadClass("com.elasticjogger.plainq.AppRunner");
 
     Object runner = runnerClass.newInstance();
     Method start = runnerClass.getMethod("start");
     start.invoke(runner);
   }
 
-  public List<URL> listJars(String dirPath) throws MalformedURLException
+  public void start() throws Exception
+  {
+    Class amqClass = mClassLoader.loadClass("org.apache.activemq.ActiveMQConnectionFactory");
+
+    ConnectionFactory factory = (ConnectionFactory) amqClass.newInstance();
+    Connection conn = factory.createConnection();
+    ConnectionMetaData metadata = conn.getMetaData();
+    System.out.println(metadata.getJMSVersion());
+  }
+
+  public static List<URL> listJars(String dirPath) throws MalformedURLException
   {
     File dir = new File(dirPath);
     File[] files = dir.listFiles();
